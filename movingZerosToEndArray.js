0ca17b9d4a3f9142011b22b0478cd9a364b8b298
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2023-12-08 16:58:44 +0000

    Amended to a negative loop and added comment descriptions

--- movingZerosToEndArray.js
+++ movingZerosToEndArray.js
@@ -2,12 +2,14 @@
 
 function moveZeros(arr) {
   //Create a loop to go through each item in the array
-  for (i = 0; i < arr.length-1; i++){
+  for (i = arr.length - 1; i >= 0; i--){
     //if the array element is 0
     if (arr[i] === 0) {
       // remove the 0 using ____ and insert it at the end using splice
-      arr.push(arr.splice(i, 1)[0]); //Not quite sure why the [0] is required, but it is. No reason it's needed in literature on splice or push.
+      arr.push(arr.splice(i, 1)[0]); // arr.splice removes 1 item in the i index position, returns an array with one item in it, hence why you need to put the [0] in.
     }
   }
   return arr;
 }
+
+//NOTE - need to loop negatively from the end to the start, as if you go positively, you will miss indexes as they are moved to the end.
